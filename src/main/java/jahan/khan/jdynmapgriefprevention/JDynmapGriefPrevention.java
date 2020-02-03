7592fbe5450028aae4646bd8906918846bd3fce3
Dmitry Rendov <drendov@gmail.com>
2020-02-03 19:59:55 +0300

    Move AreaStyle class to a separate file

--- src/main/java/jahan/khan/jdynmapgriefprevention/JDynmapGriefPrevention.java
+++ src/main/java/jahan/khan/jdynmapgriefprevention/JDynmapGriefPrevention.java
@@ -272,33 +272,6 @@ class JDynmapGriefPrevention extends JavaPlugin {
         this.numManagers = numManagers;
     }
 
-    private static class AreaStyle {
-        final String strokecolor;
-
-        final double strokeopacity;
-        final int strokeweight;
-        final String fillcolor;
-        final double fillopacity;
-        String label;
-
-        AreaStyle(FileConfiguration cfg, String path, AreaStyle def) {
-            this.strokecolor = cfg.getString(path + ".strokeColor", def.strokecolor);
-            this.strokeopacity = cfg.getDouble(path + ".strokeOpacity", def.strokeopacity);
-            this.strokeweight = cfg.getInt(path + ".strokeWeight", def.strokeweight);
-            this.fillcolor = cfg.getString(path + ".fillColor", def.fillcolor);
-            this.fillopacity = cfg.getDouble(path + ".fillOpacity", def.fillopacity);
-            this.label = cfg.getString(path + ".label", null);
-        }
-
-        AreaStyle(FileConfiguration cfg) {
-            this.strokecolor = cfg.getString("regionstyle" + ".strokeColor", "#FF0000");
-            this.strokeopacity = cfg.getDouble("regionstyle" + ".strokeOpacity", 0.8D);
-            this.strokeweight = cfg.getInt("regionstyle" + ".strokeWeight", 3);
-            this.fillcolor = cfg.getString("regionstyle" + ".fillColor", "#FF0000");
-            this.fillopacity = cfg.getDouble("regionstyle" + ".fillOpacity", 0.35D);
-        }
-    }
-
     //adds a server log entry
     private void console(String msg) {
         getServer().getConsoleSender().sendMessage("[JDynmapGriefPrevention] " + msg);
